99d31f61ae1ecd5f6a346f50e02f78a03c80b328
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-02-20 19:55:36 +0100

    changement necessaire lab00

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author olivier
+ */
+public class Flute implements IInstrument {
+
+   public String play() {
+      return "";
+   }
+
+   public String getColor() {
+      return "";
+   }
+
+   public int getSoundVolume() {
+      return 5;
+   }
+
+}
