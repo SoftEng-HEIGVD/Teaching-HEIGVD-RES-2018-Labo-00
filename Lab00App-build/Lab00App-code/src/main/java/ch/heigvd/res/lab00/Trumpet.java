a9b1480453b1609e21d9fbb80a8e96b6c2a78f43
Perceval <dnamella1@hotmail.com>
2018-02-21 15:23:41 +0100

    Trumpet class added

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,19 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Fits the TrumpetTest Requirements
+ * @author Adrien Allemand
+ */
+ 
+ public class Trumpet implements IInstrument {
+	 public Trumpet(){};
+
+	 public String play(){
+	     return "pouet";
+     }
+
+     public int getSoundVolume() {
+	     return 2;
+     }
+
+ }
