59751be4df19b2668322b77d8f76d6d7fcdc7aad
Unknown <jeremie.chatillon@heig-vd.ch>
2018-02-21 11:47:25 +0100

    Creat docs for TrumpetTest

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,23 @@
+/*
+ *      RES: Labo00
+ *      Autor: Châtillon Jérémie
+ *      Date: 21.02.2018   
+ */
+
+package ch.heigvd.res.lab00;
+
+class Flute implements IInstrument{
+    public String play(){
+        return "fluu";
+    }
+    
+    public int getSoundVolume(){
+        return 5;
+    }
+    
+    public String getColor(){
+        return "brown";
+    }
+}
+
+
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,14 @@
+/*
+ *      RES: Labo00
+ *      Autor: Châtillon Jérémie
+ *      Date: 21.02.2018   
+ */
+
+package ch.heigvd.res.lab00;
+
+interface IInstrument{
+    public String play();
+    public int getSoundVolume();
+    public String getColor();
+}
+
