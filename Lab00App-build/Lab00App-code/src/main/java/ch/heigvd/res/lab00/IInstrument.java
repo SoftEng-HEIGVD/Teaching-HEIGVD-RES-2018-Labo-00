0f0f1095cbaa9d7f9e96d6ead8cd0c1a0c7cb4b4
antoine.rochat <antoine.rochat@heig-vd.ch>
2018-02-21 10:24:16 +0100

    Implementation of xxx according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,11 @@
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Antoine Rochat
+ */
+public interface IInstrument{
+    public String play();
+    public int getSoundVolume();
+    public String getColor();
+}
