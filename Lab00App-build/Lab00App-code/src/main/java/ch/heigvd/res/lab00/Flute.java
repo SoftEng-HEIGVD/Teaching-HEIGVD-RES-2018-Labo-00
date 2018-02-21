e629b08dad95f0a8d1183e1e451166ef791d29ab
Christophe Joyet <christophe.joyet@heig-vd.ch>
2018-02-21 09:59:33 +0100

    Implementation of IInstrument Trumpet Flute according  to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,20 @@
+package ch.heigvd.res.lab00;
+
+
+class Flute implements IInstrument{
+
+
+  public int getSoundVolume(){
+  	return 1;
+  }
+
+  public String play(){
+  	return "tutut";
+  }
+
+  public String getColor(){
+  	return "brune";
+  }
+
+  
+}
