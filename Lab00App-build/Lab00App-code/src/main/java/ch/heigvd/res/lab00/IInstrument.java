7a58f7de87f618cc2e2cb28d9f092f77fac56fd3
Max Caduff <max.caduff@heig-vd.ch>
2018-02-21 09:55:03 +0100

    done

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,13 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created on 21.02.18.
+ *
+ * @author Max
+ */
+public interface IInstrument {
+
+    String play();
+    int getSoundVolume();
+    String getColor();
+}
