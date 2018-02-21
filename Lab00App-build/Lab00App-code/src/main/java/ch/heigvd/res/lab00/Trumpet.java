a12b4d90fe99fc4b60046a7c7dfc5acc45b86de0
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2018-02-21 13:39:06 +0100

    Implementation of Labo00

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,20 @@
+package ch.heigvd.res.lab00;
+
+public class Trumpet implements IInstrument {
+
+	public String play() {
+
+		return "pouet";
+	}
+
+	public int getSoundVolume() {
+
+		return 1;
+	}
+
+	public String getColor() {
+
+		return "golden";
+	}
+
+}
