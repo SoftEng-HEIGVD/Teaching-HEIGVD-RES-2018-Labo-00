0c9c8aea95d0ae9df2a20565f242d3496b796c53
cedricHEIGVD <cedriclankeu2@yahoo.fr>
2018-02-21 20:10:51 +0100

    Implementation of Lab00App-code project (Flute, Trumpet and IInstrument) according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author LNC
+ */
+public interface IInstrument 
+{
+    public int getSoundVolume();
+    public String getColor();
+    public String play();
+}
