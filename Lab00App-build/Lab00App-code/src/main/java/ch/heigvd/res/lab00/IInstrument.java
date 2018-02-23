e26f08e3e2a90a8415fc6498325f5ff909ef065e
Ludwig9392 <loic.frueh@gmail.com>
2018-02-23 09:26:45 +0100

    Implementation of Labo00 according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,13 @@
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Frueh Loïc
+ */
+public interface IInstrument {
+   
+   public String play();
+   public int getSoundVolume();
+   public String getColor();
+   
+}
