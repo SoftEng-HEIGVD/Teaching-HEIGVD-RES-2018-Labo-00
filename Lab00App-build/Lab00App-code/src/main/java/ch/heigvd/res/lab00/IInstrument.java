d3eab2e1a9a8e4320297bb036eb28ab40036c75a
SashaKor <alexandra.korukova@gmail.com>
2018-02-26 10:37:36 +0100

    Application.java added

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.res.lab00;
 
 /**
  *
- * @author Sashok
+ * @author Alexandra Korukova
+ *
  */
 public interface IInstrument {
     String play();
